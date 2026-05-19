d4f6cf987f39dadf160718400d31495f312e6a7d
lmoix <lmoix@devesa.com>
2026-05-19 12:56:16 -0300

    feat (back): update incident_log services

--- backend/apps/logs/services.py
+++ backend/apps/logs/services.py
@@ -27,3 +27,36 @@ def log_incident_reassigned(incident: Incident, user) -> IncidentLog:
         previous_satatus= Incident.STATUS_IN_PROGRESS
     )
 
+
+def log_status_change(
+    incident: Incident,
+    user,
+    previous_status: str,
+    new_status: str,
+) -> IncidentLog:
+    return IncidentLog.objects.create(
+        incident= incident,
+        user= user,
+        action= IncidentLog.ACTION_STATUS_CHANGE,
+        previous_status= previous_status,
+        new_status= new_status,
+    )
+
+def log_incident_updated(incident: Incident, user) -> IncidentLog:
+        return IncidentLog.objects.create(
+        incident= incident,
+        user= user,
+        action= IncidentLog.ACTION_UPDATE,
+        previous_status= None,
+        new_status= None,
+    )
+
+
+def log_incident_closed(incident: Incident, user) -> IncidentLog:
+    return IncidentLog.objects.create(
+        incident= incident,
+        user= user,
+        action= IncidentLog.ACTION_CLOSE,
+        previous_status= Incident.STATUS_IN_PROGRESS,
+        new_status= Incident.STATUS_CLOSED,
+    )
