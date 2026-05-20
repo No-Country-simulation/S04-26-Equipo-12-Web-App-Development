bc784f46bbdda304efb472a9f550b0eeed31fb0f
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2026-05-20 18:53:05 -0300

    feat: include theme provider and theme toggle

--- frontend/opscore-web/src/App.tsx
+++ frontend/opscore-web/src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { LoginPage } from "./pages/LoginPage";
-
+import { useTheme } from "./utils/useTheme";
+import { ThemeToggle } from "./components/atoms/ThemeToggle";
 
 function App() {
-
+  useTheme();
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <div className="fixed top-4 right-4 z-50">
+        <ThemeToggle />
+      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
