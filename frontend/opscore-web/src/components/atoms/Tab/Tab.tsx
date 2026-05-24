54675e79ab58e841198927741114fd8c7353cfd1
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2026-05-24 12:49:40 -0300

    feat: include default styles for tab

--- frontend/opscore-web/src/components/atoms/Tab/Tab.tsx
+++ frontend/opscore-web/src/components/atoms/Tab/Tab.tsx
@@ -3,17 +3,26 @@ import { type LucideIcon } from 'lucide-react'
 import { cn } from '@/utils/cn'
 
 
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: Variant
-  size?: Size
   loading?: boolean
   fullWidth?: boolean
   leftIcon?: LucideIcon
   rightIcon?: LucideIcon
 }
 
-export const Tab = ({children, ...props}: ButtonProps) => {
+export const Tab = ({children, className, ...props}: ButtonProps) => {
   return (
-    <button {...props}>{children}</button>
+    <button 
+      className={cn(
+        'w-full p-12 mb-2 text-sm text-center border-2 border-primary-700 rounded-tl-lg rounded-tr-lg',
+        'transition-colors duration-150',
+        'active:bg-primary-700 active:text-secondary-500',
+        'hover:bg-primary-600 hover:text-secondary-500',
+        className,
+      )} 
+      {...props}>
+      {children}
+    </button>
   )
 }
